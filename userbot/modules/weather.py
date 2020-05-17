10dfcf4c130d4974ea6963d90f085dc3b09f99a8
fadlyas07 <fadlyardhians@gmail.com>
2020-05-17 20:23:02 +0700

    weather: update output message
    Signed-off-by: fadlyas07 <fadlyardhians@gmail.com>

--- userbot/modules/weather.py
+++ userbot/modules/weather.py
@@ -118,16 +118,16 @@ async def get_weather(weather):
         return xx
 
     await weather.edit(
-        f"**Temperature:** `{celsius(curtemp)}°C | {fahrenheit(curtemp)}°F`\n"
+        f"**Temperature :** `{celsius(curtemp)}°C | {fahrenheit(curtemp)}°F`\n"
         +
-        f"**Min. Temp.:** `{celsius(min_temp)}°C | {fahrenheit(min_temp)}°F`\n"
+        f"**Min. Temp. :** `{celsius(min_temp)}°C | {fahrenheit(min_temp)}°F`\n"
         +
-        f"**Max. Temp.:** `{celsius(max_temp)}°C | {fahrenheit(max_temp)}°F`\n"
-        + f"**Humidity:** `{humidity}%`\n" +
-        f"**Wind:** `{kmph[0]} kmh | {mph[0]} mph, {findir}`\n" +
-        f"**Sunrise:** `{sun(sunrise)}`\n" +
-        f"**Sunset:** `{sun(sunset)}`\n\n" + f"**{desc}**\n" +
-        f"`{cityname}, {fullc_n}`\n" + f"`{time}`")
+        f"**Max. Temp. :** `{celsius(max_temp)}°C | {fahrenheit(max_temp)}°F`\n"
+        + f"**Humidity :** `{humidity}%`\n" +
+        f"**Wind :** `{kmph[0]} kmh | {mph[0]} mph, {findir}`\n" +
+        f"**Sunrise :** `{sun(sunrise)}`\n" +
+        f"**Sunset :** `{sun(sunset)}`\n\n" + f"**{desc}**\n" +
+        f"📍`{cityname}, {fullc_n}`\n" + f"⏱`{time}`")
 
 
 CMD_HELP.update({
